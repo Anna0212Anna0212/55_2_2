7e986813a53103b8a7d495146914363cf3a801a2
揚恩 張 <anna0983266723@gmail.com>
2025-02-11 18:12:13 +0800

    加入專案檔案。

---
+++ 55_2_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("輸入第一個數字(0-100)：");
+                string onee = Console.ReadLine();
+                Console.Write("輸入第二個數字(0-100)：");
+                string twoo = Console.ReadLine();
+                int one = int.Parse(onee);
+                int two = int.Parse(twoo);
+                if(one>100||one<0||two<0||two>100)
+                {
+                    Console.WriteLine("error");
+                }
+                else
+                {
+                    int max = one > two ? one : two;
+                    int min = one > two ? two : one;
+                    for (int i = min; i > 0; i--)
+                    {
+                        if (max % i == 0 && min % i == 0)
+                        {
+                            Console.WriteLine("最大公因數為："+i);
+                            Console.Write($"最小公倍數為：{(max/i)*(min/i)*i}");
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
