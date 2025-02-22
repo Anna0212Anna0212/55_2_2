f596832d9c1eac0621798ec793cca9a0874440a9
揚恩 張 <anna0983266723@gmail.com>
2025-02-22 16:41:41 +0800

    1

--- 55_2_2/Program.cs
+++ 55_2_2/Program.cs
@@ -8,6 +8,7 @@ namespace _55_2_2
 {
     internal class Program
     {
+        public static bool trfa = true;
         static void Main(string[] args)
         {
             try
@@ -18,7 +19,7 @@ namespace _55_2_2
                 string twoo = Console.ReadLine();
                 int one = int.Parse(onee);
                 int two = int.Parse(twoo);
-                if(one>100||one<0||two<0||two>100)
+                if (one > 100 || one < 0 || two < 0 || two > 100)
                 {
                     Console.WriteLine("error");
                 }
@@ -30,8 +31,8 @@ namespace _55_2_2
                     {
                         if (max % i == 0 && min % i == 0)
                         {
-                            Console.WriteLine("最大公因數為："+i);
-                            Console.Write($"最小公倍數為：{(max/i)*(min/i)*i}");
+                            Console.WriteLine("最大公因數為：" + i);
+                            Console.Write($"最小公倍數為：{(max / i) * (min / i) * i}");
                             break;
                         }
                     }
@@ -40,9 +41,54 @@ namespace _55_2_2
                     {
                         if (max % i == 0 && min % i == 0)
                         {
-                             Console.Write(i+" ");
+                            Console.Write(i + " ");
                         }
-                    }   
+                    }
+                    Console.Write($"\n{min}公因數為：");
+                    for (int i = min; i > 0; i--)
+                    {
+                        if (min % i == 0)
+                        {
+                            Console.Write(i + " ");
+                        }
+                    }
+                    Console.Write($"\n{max}公因數為：");
+                    for (int i = max; i > 0; i--)
+                    {
+                        if (max % i == 0)
+                        {
+                            Console.Write(i + " ");
+                        }
+                    }
+                    Console.Write($"\n短除法：\n");
+                    int j = 2;
+                    for (int i = 2; i < min; i++)
+                    {
+                        j = 2;
+                        trfa = true;
+                        do
+                        {
+                            if (i % j == 0 || j >= i)
+                            {
+                                trfa = false;
+                            }
+                            j++;
+                        } while (trfa);
+                        if (trfa)
+                        {
+                            while (min % i == 0 && max % i == 0)
+                            {
+                                for(int k = 0; k < i; k++)
+                                {
+                                    Console.Write(" ");
+                                }
+                                Console.Write($"{min % i} | {max}  {min}");
+                                Console.Write("___________");
+                                min /= i;
+                                max /= i;
+                            }
+                        }
+                    }
                 }
             }
             catch
